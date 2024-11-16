9d5f8e6c8577ae9381751e633675366ca6ef6737
zakriahayder <zakriahayder12@gmail.com>
2024-11-16 15:19:41 -0500

    Finished all the functionality, just need to replace the placeholders

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from helpers import load_pedestrian_data
 
 app = Flask(__name__)
+app.secret_key = '12345'
 
 ped = load_pedestrian_data("data.csv")
 
@@ -9,8 +10,9 @@ ped = load_pedestrian_data("data.csv")
 @app.route('/live-data')
 def live_data():
     # Replace with actual logic to display live data
-
     return render_template('index.html', ped=ped)
+
+
 
 @app.route('/data')
 def about():
