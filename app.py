1c570d5231a9ec0d6d7d1097676355ce1cda5268
zakriahayder <zakriahayder12@gmail.com>
2024-11-16 13:28:58 -0500

    Made some changes

--- app.py
+++ app.py
@@ -2,13 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/live-data')
 def live_data():
     # Replace with actual logic to display live data
-    return render_template('index.html')
+    ped = {
+        "12:00": 69,
+        "13:00": 45,
+        "14:00": 30,
+        "15:00": 50,
+        "16:00": 80,
+    }
+    
+    return render_template('index.html', ped=ped)
 
-# @app.route('/about')
-# def about():
-#     # Replace with your "About" page content
-#     return render_template('about.html', title="About")
+@app.route('/about')
+def about():
+    # Replace with your "About" page content
+    return render_template('about.html')
