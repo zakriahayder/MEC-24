65bc426a24ecaac8de6d5ecb0fbdb1c8d3407c10
zakriahayder <zakriahayder12@gmail.com>
2024-11-16 15:43:06 -0500

    Two placeholders need to be fixed

--- app.py
+++ app.py
@@ -7,14 +7,13 @@ app.secret_key = '12345'
 ped = load_pedestrian_data("data.csv")
 
 @app.route('/')
-@app.route('/live-data')
 def live_data():
     # Replace with actual logic to display live data
-    return render_template('index.html', ped=ped)
+    return render_template('data.html', ped=ped)
 
 
 
 @app.route('/data')
 def about():
     # Replace with your "About" page content
-    return render_template('data.html', ped=ped)
+    return render_template('index.html', ped=ped)
