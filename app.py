f6a5761e7bafd3cdde4363a59ca8b13ec27c613c
Bryan Kikuta <112445285+kikutabryan@users.noreply.github.com>
2024-11-17 09:41:36 -0500

    changed name

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ from flask import Flask, render_template, flash
 from helpers import load_pedestrian_data
 
 app = Flask(__name__)
-app.secret_key = '12345'
+app.secret_key = "12345"
 
 ped = load_pedestrian_data("data.csv")
 
-@app.route('/')
+
+@app.route("/")
 def live_data():
     # Replace with actual logic to display live data
-    return render_template('data.html', ped=ped)
+    return render_template("data.html", ped=ped)
 
 
-
-@app.route('/data')
+@app.route("/data")
 def about():
     # Replace with your "About" page content
-    return render_template('index.html', ped=ped)
+    return render_template("index.html", ped=ped)
